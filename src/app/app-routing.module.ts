3ec26596cdcaca5467d1b9e7aac91d6920be89d4
pradeep <pradeepkumar.pk002@gmail.com>
2022-06-14 18:12:35 +0530

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { Section2Component } from './section2/section2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SlideComponent } from './slide/slide.component';
+import { FooterComponent } from './footer/footer.component';
+import { Section1Component } from './section1/section1.component';
+import { MainComponent } from './main/main.component';
+import { Section2Component } from './section2/section2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SlideComponent,
+    FooterComponent,
+    Section1Component,
+    MainComponent,
+    Section2Component
   ],
   imports: [
     BrowserModule,
